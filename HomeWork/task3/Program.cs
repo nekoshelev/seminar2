a43f027dd634853257e1335af3a08eb172fb38d2
«Nikolay <>
2022-07-02 23:41:00 +0600

    дом. работа 3

---
+++ HomeWork/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int n = Prompt("Введите трехзначное число: ");
+int free = 10;
+
+if (n > 99)
+{
+    Console.WriteLine(n.ToString()+" = "+n.ToString()[2]);
+}
+else Console.WriteLine("Третьей цифры нету");
