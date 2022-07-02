93a64c7c99eac413368886978879d29121ddec41
«Nikolay <>
2022-07-02 22:04:50 +0600

    задача5

---
+++ task5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 9999] и показывает наибольшую цифру числа.
+//7812 -> 8 
+//1213-> 3 
+//845 -> 8
+
+int rnd (int min, int max)
+{
+Random rnd = new Random();
+return rnd.Next(min, max);
+}
+
+int a = rnd (10, 9999);
+Console.WriteLine(a);
+
+int max = 0;
+
+while (a>0)
+{
+    int tmp = a%10;
+    if(tmp > max) max = tmp;
+    a = a/10;
+}
+
+  Console.WriteLine(max); 
